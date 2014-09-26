8ade1d9626601c30438032b480cc0e91bb3699a5
Dennis van der Vliet <dennis@thenextweb.com>
2014-09-26 16:59:44 +0200

    Add public methods to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,14 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:redbooth]
 
+  scope :active, -> { where.not(project_id: nil, tasklist_id: nil, authentication_token: nil).where("token_expires_at >= ?", Time.now) }
+
   def self.from_omniauth(auth)
     user = where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
     end
-    user.update_attributes(authentication_token: auth.credentials.token, token_expires_at: Time.at(auth.credentials.expires_at).to_datetime)
+    user.update_attributes(authentication_token: auth.credentials.token, token_expires_at: auth.credentials.expires_at ? Time.at(auth.credentials.expires_at) : Time.now + 2.hours)
     user
   end
 
@@ -17,16 +19,21 @@ class User < ActiveRecord::Base
   end
 
   def tasklists
-    api.tasks
+    api.tasklists
   end
 
   def projects
-    api.tasks
+    api.projects
   end
 
   def me
     api.me
   end
+
+  def active?
+    token_expires_at != nil && token_expires_at > Time.now
+  end
+
   def api
     @api ||= API::User.new(self)
   end
