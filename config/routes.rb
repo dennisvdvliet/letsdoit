028037a02bc4d63797f56f560a2db337b33ac508
Dennis van der Vliet <dennis@thenextweb.com>
2014-09-27 18:10:41 +0200

    New root page and Trello callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for  :users,
+              :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  devise_scope :user do
+    root "devise/sessions#new"
+    get "users/auth/trello/callback", :controller => "users/omniauth_callbacks", :action => "trello"
+  end
 
   resources :tasklists
   resources :projects
 
   post :dashboard, controller: :dashboard, action: :update
   get :dashboard, controller: :dashboard, action: :show
-
-  devise_scope :user do
-    get "users/auth/trello/callback", :controller => "users/omniauth_callbacks", :action => "trello"
-  end
 end
